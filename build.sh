f1de99217cf2f20d04e5d20986f003f024c9d9ea
Eric Cousineau <eric.cousineau@tri.global>
2018-12-16 14:47:17 -0500

    Start hacking

--- build.sh
+++ build.sh
@@ -16,31 +16,40 @@ export-prepend ()
     eval "export $1=\"$2:\$$1\""
 }
 
-rm -rf build
+cur=$(cd $(dirname $0) && pwd)
+# rm -rf build
 mkcd build
 
-mkdir install
+mkdir -p install
 mkdir -p install/{bin,lib}
 fhs-extend ~+/install
 
 set -eux
 
 (
-    mkcd download
+    if [[ ! -d ${cur}/download ]]; then
+        mkcd ${cur}/download
+        curl -L https://astuteinternet.dl.sourceforge.net/project/ispcmirror/v1.9.2/ispc-v1.9.2-linux.tar.gz -O
+    fi
+)
 
-    curl -L https://astuteinternet.dl.sourceforge.net/project/ispcmirror/v1.9.2/ispc-v1.9.2-linux.tar.gz -O
-    tar xfz ispc-v1.9.2-linux.tar.gz -C ../install/bin ispc-v1.9.2-linux/ispc --strip-components=1
+(
+    tar xfz ${cur}/download/ispc-v1.9.2-linux.tar.gz -C install/bin ispc-v1.9.2-linux/ispc --strip-components=1
 )
 
 (
     mkcd embree
     # https://github.com/embree/embree/issues/190
     cmake ../../embree \
+        -DCMAKE_BUILD_TYPE=Debug \
         -DCMAKE_INSTALL_PREFIX=../install \
-        -DEMBREE_TUTORIALS=OFF \
-        -DEMBREE_ISA_AVX512KNL=OFF \
-        -DEMBREE_ISA_AVX512SKX=OFF \
-        -DEMBREE_TASKING_SYSTEM=INTERNAL
+        $(echo "
+        -DBUILD_SHARED_LIBS=ON
+        -DBUILD_TESTING=OFF
+        -DEMBREE_MAX_ISA=SSE4.2
+        -DEMBREE_STACK_PROTECTOR=ON
+        -DEMBREE_TUTORIALS=OFF
+        ")
     make -j install
 )
 
@@ -49,7 +58,11 @@ set -eux
     cmake ../../ospray \
         -DCMAKE_INSTALL_PREFIX=../install \
         -DCMAKE_BUILD_TYPE=Debug \
-        -DOSPRAY_TASKING_SYSTEM=INTERNAL
+        $(echo "
+        -DBUILD_SHARED_LIBS=ON
+        -DOSPRAY_ENABLE_APPS=OFF
+        -DOSPRAY_ENABLE_TESTING=OFF
+        ")
     make -j install
 )
 
@@ -58,27 +71,29 @@ set -eux
     mkcd vtk
     cmake ../../vtk \
         -DCMAKE_INSTALL_PREFIX=../install \
-        -DBUILD_TESTING=OFF \
-        -DBUILD_SHARED_LIBS=ON \
         -DCMAKE_BUILD_TYPE=Debug \
-        -DModule_vtkRenderingOSPRay=ON \
-        -DOSPRAY_INSTALL_DIR=../install \
-        -DVTK_Group_Qt=ON \
-        -DVTK_LEGACY_REMOVE=ON \
-        -DVTK_QT_VERSION=5 \
-        -DVTK_USE_SYSTEM_EXPAT=ON \
-        -DVTK_USE_SYSTEM_FREETYPE=ON \
-        -DVTK_USE_SYSTEM_HDF5=ON \
-        -DVTK_USE_SYSTEM_JPEG=ON \
-        -DVTK_USE_SYSTEM_JSONCPP=ON \
-        -DVTK_USE_SYSTEM_LIBXML2=ON \
-        -DVTK_USE_SYSTEM_LZ4=ON \
-        -DVTK_USE_SYSTEM_NETCDF=ON \
-        -DVTK_USE_SYSTEM_NETCDFCPP=ON \
-        -DVTK_USE_SYSTEM_OGGTHEORA=ON \
-        -DVTK_USE_SYSTEM_PNG=ON \
-        -DVTK_USE_SYSTEM_TIFF=ON \
-        -DVTK_USE_SYSTEM_ZLIB=ON \
+        $(echo "
+        -DBUILD_SHARED_LIBS=ON
+        -DBUILD_TESTING=OFF
+        -DModule_vtkRenderingOSPRay=ON
+        -DVTK_ENABLE_VTKPYTHON=OFF
+        -DVTK_Group_Qt=ON
+        -DVTK_LEGACY_REMOVE=ON
+        -DVTK_QT_VERSION=5
+        -DVTK_USE_SYSTEM_EXPAT=ON
+        -DVTK_USE_SYSTEM_FREETYPE=ON
+        -DVTK_USE_SYSTEM_HDF5=ON
+        -DVTK_USE_SYSTEM_JPEG=ON
+        -DVTK_USE_SYSTEM_JSONCPP=ON
+        -DVTK_USE_SYSTEM_LIBXML2=ON
+        -DVTK_USE_SYSTEM_LZ4=ON
+        -DVTK_USE_SYSTEM_NETCDF=ON
+        -DVTK_USE_SYSTEM_NETCDFCPP=ON
+        -DVTK_USE_SYSTEM_OGGTHEORA=ON
+        -DVTK_USE_SYSTEM_PNG=ON
+        -DVTK_USE_SYSTEM_TIFF=ON
+        -DVTK_USE_SYSTEM_ZLIB=ON
         -DVTK_WRAP_PYTHON=ON
+        ")
     make -j install
 )
